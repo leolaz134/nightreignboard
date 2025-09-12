64e75804d6ce1ab179a32152b0e001684c1b355a
sbblacknight <something4529@gmail.com>
2025-09-12 20:54:31 +1200

    Added more direct CORS config file to map react response as an OPTION, also fix url links for service file

--- nightreignboard-frontend/src/service/PostService.js
+++ nightreignboard-frontend/src/service/PostService.js
@@ -1,15 +1,13 @@
 import axios from 'axios';
 
-const API_URL = 'http://localhost:9090/api/posts';
-
 class  PostService {
     // Fetch all users
     getPosts() {
-        return axios.get(API_URL);
+        return axios.get('http://localhost:9090/posts/getAllPosts');
     }
     
     createPost(post) {
-        return axios.post('http://localhost:9090/api/posts/addPost', post);
+        return axios.post('http://localhost:9090/posts/addPost', post);
     }
 }
 
--- src/main/java/com/example/nightreignboard/controller/PostController.java
+++ src/main/java/com/example/nightreignboard/controller/PostController.java
@@ -11,21 +11,21 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 
-@CrossOrigin(origins = "http://localhost:3000")
 @RestController
+@RequestMapping("/posts")
 public class PostController {
     
     @Autowired
     private PostRepo postRepo;
 
-    @GetMapping("/api/posts")
+    @GetMapping("/getAllPosts")
     public ResponseEntity<List<Post>> getAllPosts(){
         try{
             List<Post> postList = new ArrayList<>();
